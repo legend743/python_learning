ff6f33464cea277103837bee7fc407d5cb985c43
faisal <fmohd743@gmail.com>
2023-11-04 19:05:18 +0530

    Recursion and Tower of Hanoi

---
+++ Stack/recursiion.py
@@ -0,0 +1,11 @@
+n=int(input("enter the number"))
+m=int(input("enter the power"))
+def power(n,m):
+    if m==1:
+        return n
+    else:
+        return n*power(n,m-1)
+
+p=power(m,n)
+print(p)
+         
