00d637f2e8edb662409c0647c7e3577a22987d61
faisal <fmohd743@gmail.com>
2023-12-21 15:27:43 +0530

    lambdafuntion

--- sumtarget9.py
+++ sumtarget9.py
@@ -1,17 +1,25 @@
-# l=[10,2,3,4,5,6]
-# for i in range(len(l)-1):
-#     print(l[i])
+# # l=[10,2,3,4,5,6]
+# # for i in range(len(l)-1):
+# #     print(l[i])
  
-#     for j in range (1,len(l)-1):
-#         print("this is my l wali string",l[j])
+# #     for j in range (1,len(l)-1):
+# #         print("this is my l wali string",l[j])
+# #         if(l[i]+l[j]==9):
+# #             print("we got the string")
+# #         else:
+# #             print("continue")
+# nums = [2,7,11,15]
+# for i in range(len(l)-1):
+#     for j in range(1,len(l)-1):
 #         if(l[i]+l[j]==9):
-#             print("we got the string")
+#             return 1
 #         else:
-#             print("continue")
-nums = [2,7,11,15]
-for i in range(len(l)-1):
-    for j in range(1,len(l)-1):
-        if(l[i]+l[j]==9):
-            return 1
-        else:
-            return 0
+#             return 0
+def square(x):
+    return x**2
+def operates_on_number(func,num):
+    result=func(num)
+    return result
+number=10
+result=operates_on_number(square,number)
+print(f"the square of:{number} is:{result} ")
